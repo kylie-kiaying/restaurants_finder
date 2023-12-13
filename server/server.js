6d7c1585a567281de9c53202c8a030c5afe29c0b
kylie-kiaying <kylie.kiayingchua@gmail.com>
2023-12-13 22:27:27 +0800

    express setup

---
+++ server/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require("express")
+const app = express()
+
+app.get("/getRestaurants", (req, res) => {
+    res.status(404).json({
+        status: "success",
+        restaurant: "mcdonalds",
+    });
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`server is up and listening on port ${port}`);
+});
