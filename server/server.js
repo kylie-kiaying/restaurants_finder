6168a324a293dc3fc6a17f89e3b8813acff00341
kylie-kiaying <kylie.kiayingchua@gmail.com>
2023-12-14 18:20:58 +0800

    Set up routers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,61 @@
-require('dotenv').config()
-const express = require("express")
-const app = express()
+require('dotenv').config();
+const express = require("express");
+const morgan = require("morgan")
+const app = express();;
 
-app.get("/getRestaurants", (req, res) => {
-    res.status(404).json({
+app.use(express.json());
+
+//Get all Restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+    console.log("route handler ran");
+    res.status(200).json({
         status: "success",
-        restaurant: "mcdonalds",
+        data:{
+            restaurant: ["mcdonalds", "wendys"],
+        }
+    });
+});
+
+//Get a Restaurant
+app.get("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params);
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: "mcdonalds"
+        }
+    })
+});
+
+//Create a Restaurant
+app.post("/api/v1/restaurants", (req, res) => {
+    console.log(req.body);
+    res.status(201).json({
+        status: "success",
+        data: {
+            restaurant: "mcdonalds"
+        }
+    })
+});
+
+//Update Restaurants
+
+app.put("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: "mcdonalds",
+        }
+    });
+});
+
+//Delete Restaurant
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+    res.status(204).json({
+        status: "success"
     });
 });
 
